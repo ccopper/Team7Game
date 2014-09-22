7cb76c93f615e0db9d9599308a84d8806d6ca071
ccopper <ccopper@mymail.mines.edu>
2014-09-22 11:35:13 -0600

    Added base files

---
+++ Game.js
@@ -0,0 +1,29 @@
+/*
+
+*/
+(function()
+{
+
+$( document ).ready(function() 
+{
+	
+	
+	
+	$("table td").hover(function()
+	{	
+		$(this).addClass("cellRotate");
+
+	}, function()
+	{
+		$(this).removeClass("cellRotate");
+	});
+	
+	
+	
+});
+
+
+
+
+
+})();
